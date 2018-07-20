caf69fb95d8c4db6d218150e7633240c6a28672d
Christopher Wong <cwong8257@gmail.com>
2018-07-20 17:29:28 -0400

    Add bodyparser and issue, client, user models

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 
-const sequelize = new Sequelize('tissues-db', 'oreo', 'cookie', {
+const sequelize = new Sequelize('tissuesdb', 'oreo', 'cookie', {
   host: 'localhost',
   dialect: 'postgres',
 });
 
+const Issue = sequelize.define('issue', {
+  _id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  status: { type: Sequelize.ENUM('In progess', 'Completed'), defaultValue: 'In progress' },
+  description: Sequelize.STRING,
+  createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+  start: Sequelize.DATE,
+  end: Sequelize.DATE,
+});
+
+const Client = sequelize.define('client', {
+  username: { type: Sequelize.STRING, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false },
+  domain: { type: Sequelize.STRING },
+});
+
+const User = sequelize.define('user', {
+  email: { type: Sequelize.STRING, allowNull: false },
+});
+
+Client.hasMany(Issue);
+User.hasMany(Issue);
+
+sequelize.sync({ logging: console.log });
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'GET is working!' });
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/test', (req, res) => {
-  res.json({ message: 'POST is working!' });
+app.post('/api/issues', async (req, res) => {
+  try {
+    // const issue = Issue.build(req.body);
+    // await issue.save();
+    res.json(req.body);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
