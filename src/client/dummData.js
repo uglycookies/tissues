46a175067e59f10c8daddc170d9a129472ee02da
Christopher Wong <cwong8257@gmail.com>
2018-07-21 15:12:46 -0400

    Fix signup and login post request on frontend

--- src/client/dummData.js
+++ src/client/dummData.js
@@ -1,4 +1,4 @@
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat
 
 /*
 var javaScriptRelease = new Date('04 Dec 1995 00:12:00 GMT');
@@ -6,48 +6,47 @@ console.log(new Intl.DateTimeFormat('en-US').format(javaScriptRelease));
 */
 
 export const openIssues = [
-	{
-		date:'17 July 2018 00:12:00 GMT',
-	 	count:3
-	},
-	{
-		date:'18 July 2018 00:12:00 GMT',
-	 	count:4
-	},
-	{
-		date:'19 July 2018 00:12:00 GMT',
-	 	count:10
-	},
-	{
-		date:'20 July 2018 00:12:00 GMT',
-	 	count:3
-	},
-	{
-		date:'21 July 2018 00:12:00 GMT',
-	 	count:14
-	},
-]
-
+  {
+    date: '17 July 2018 00:12:00 GMT',
+    count: 3,
+  },
+  {
+    date: '18 July 2018 00:12:00 GMT',
+    count: 4,
+  },
+  {
+    date: '19 July 2018 00:12:00 GMT',
+    count: 10,
+  },
+  {
+    date: '20 July 2018 00:12:00 GMT',
+    count: 3,
+  },
+  {
+    date: '21 July 2018 00:12:00 GMT',
+    count: 14,
+  },
+];
 
 export const closedIssues = [
-	{
-		date:'17 July 2018 00:12:00 GMT',
-		count:3
-	},
-	{
-		date:'18 July 2018 00:12:00 GMT',
-		count:4
-	},
-	{
-		date:'19 July 2018 00:12:00 GMT',
-		count:10
-	},
-	{
-		date:'20 July 2018 00:12:00 GMT',
-		count:3
-	},
-	{
-		date:'21 July 2018 00:12:00 GMT',
-		count:14
-	}
-]
+  {
+    date: '17 July 2018 00:12:00 GMT',
+    count: 3,
+  },
+  {
+    date: '18 July 2018 00:12:00 GMT',
+    count: 4,
+  },
+  {
+    date: '19 July 2018 00:12:00 GMT',
+    count: 10,
+  },
+  {
+    date: '20 July 2018 00:12:00 GMT',
+    count: 3,
+  },
+  {
+    date: '21 July 2018 00:12:00 GMT',
+    count: 14,
+  },
+];
--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OpenIssues from './openIssues.js';
-import Login from './login'
-export  default class Index extends React.Component{
-	render(){
-		return <div>
-		  <h1> Hello World</h1>
-			<Login />
-			<OpenIssues />
-		</div>
-	}
+import Login from './login';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World</h1>
+        <Login />
+        <OpenIssues />
+      </div>
+    );
+  }
 }
 //
-ReactDOM.render(<Index />, document.getElementById('app'))
+ReactDOM.render(<Index />, document.getElementById('app'));
